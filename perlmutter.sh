5c57a832c9a9a35e681f644650aada6a4a8ad9be
Kevin Huck <khuck@cs.uoregon.edu>
2024-04-19 11:27:22 -0700

    Adding test on perlmutter

---
+++ perlmutter.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -e
+
+
+dokokkos() {
+rm -rf build
+set -x
+cmake -B build \
+-DCMAKE_BUILD_TYPE=RelWithDebInfo \
+-DCMAKE_CXX_COMPILER=g++ \
+-DCMAKE_CXX_COMPILER=gcc \
+-DCMAKE_INSTALL_PREFIX=`pwd`/install \
+-DKokkos_ENABLE_TUNING=ON \
+-DKokkos_ENABLE_OPENMP=ON \
+-DKokkos_ENABLE_SERIAL=ON \
+-DKokkos_ENABLE_CUDA=ON \
+-DKokkos_ENABLE_CUDA_LAMBDA=ON \
+-DKokkos_ARCH_AMPERE80=ON \
+-DKokkos_ENABLE_AGGRESSIVE_VECTORIZATION=ON \
+-DKokkos_ARCH_NATIVE=ON \
+-DKokkos_ENABLE_COMPILER_WARNINGS=ON \
+-DKokkos_ENABLE_DEPRECATED_CODE_4=ON \
+-DKokkos_ENABLE_DEPRECATION_WARNINGS=OFF \
+-DAPEX_WITH_CUDA=TRUE \
+-DAPEX_WITH_PLUGINS=TRUE \
+-DCUDAToolkit_ROOT=${CUDATOOLKIT_HOME} \
+.
+
+cmake --build build --parallel 16
+cmake --build build --parallel --target install
+cmake --build build --target tuning.tests
+}
+
+dokokkos
+
