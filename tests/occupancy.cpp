757de5ef98409e578a153dc2288545493a1b1c6a
Kevin Huck <khuck@cs.uoregon.edu>
2024-09-10 16:19:16 -0700

    Cleaning up occupancy example

--- tests/occupancy.cpp
+++ tests/occupancy.cpp
@@ -28,6 +28,23 @@
 #include <iostream>
 #include <random>
 #include <tuple>
+
+bool check_tuning(void) {
+    // surely there is a way to get this from Kokkos?
+    bool tuning = false;
+    bool internal_tuning = Kokkos::tune_internals();
+    char * tmp{getenv("APEX_KOKKOS_TUNING")};
+    if (tmp != nullptr) {
+        std::string tmpstr {tmp};
+        if (tmpstr.compare("1") == 0) {
+            tuning = true;
+        }
+    }
+    if (tuning && internal_tuning) { std::cout << "Tuning!" << std::endl; return true; }
+    std::cout << "Not Tuning!" << std::endl;
+    return false;
+}
+
 int main(int argc, char *argv[]) {
     using exec_space =  Kokkos::DefaultExecutionSpace;
     using memory_space = typename exec_space::memory_space;
@@ -37,14 +54,14 @@ int main(int argc, char *argv[]) {
   Kokkos::initialize(argc, argv);
   {
     Kokkos::print_configuration(std::cout, false);
+    bool tuning = check_tuning();
     view_type left("process_this", 1000000, 25);
     for (int i = 0 ; i < Impl::max_iterations ; i++) {
         Kokkos::RangePolicy<> p(0, left.extent(0));
         auto const p_occ = Kokkos::Experimental::prefer(
             p, Kokkos::Experimental::DesiredOccupancy{Kokkos::AUTO});
         const int M = left.extent_int(1);
-        Kokkos::parallel_for("Bench", p_occ,
-            KOKKOS_LAMBDA(int i) {
+        const auto kernel = KOKKOS_LAMBDA(int i) {
                 for (int r = 0; r < 25; r++) {
                     double f = 0.;
                     for (int m = 0; m < M; m++) {
@@ -52,8 +69,12 @@ int main(int argc, char *argv[]) {
                         left(i, m) += f;
                     }
                 }
-            }
-        );
+            };
+        if (tuning) {
+            Kokkos::parallel_for("Bench", p_occ, kernel);
+        } else {
+            Kokkos::parallel_for("Bench", p, kernel);
+        }
     }
   }
   Kokkos::finalize();
